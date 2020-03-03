edd7b8d29780b88f06789e749a192804db7dd816
Nguyen Thanh Hau <thanhhau097@gmail.com>
2020-03-03 20:08:35 +1100

    update PATH

--- temp.py
+++ temp.py
@@ -12,8 +12,8 @@ if __name__ == '__main__':
     # os.environ['CUDA_VISIBLE_DEVICES'] = '0'
     # device = torch.device('cuda:0')
     x = torch.rand((1, 4, 128, 128, 128))  # [bsize,channels,Height,Width,Depth]
-    # dmf = DMFNet(c=4, groups=16, norm='sync_bn', num_classes=4)
-    # dmfpp = DMFNet_pp(c=4, groups=16, norm='sync_bn', num_classes=4)
+    dmf = DMFNet(c=4, groups=16, norm='sync_bn', num_classes=4)
+    dmfpp = DMFNet_pp(c=4, groups=16, norm='sync_bn', num_classes=4)
     dmffull = DMFNet_fullpp(c=4, groups=16, norm='sync_bn', num_classes=4)
     # dmffull.cuda(device)
     # y = dmffull(x)
@@ -23,6 +23,6 @@ if __name__ == '__main__':
     def count_parameters(model):
         return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-    # print(count_parameters(dmf))
-    # print(count_parameters(dmfpp))
+    print(count_parameters(dmf))
+    print(count_parameters(dmfpp))
     print(count_parameters(dmffull))
