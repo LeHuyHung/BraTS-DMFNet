003aa1e094770c2f5c9a912edc86a3bd435e633f
Nguyen Thanh Hau <thanhhau097@gmail.com>
2020-03-16 17:09:39 +1100

    add config

--- temp.py
+++ temp.py
@@ -5,18 +5,19 @@ from models.unetpp.DMFNet_ppd import DMFNet_ppd
 from models.unetpp.DMFNet_fullpp import DMFNet_fullpp
 from models.attention_unet.DMFNet_attention import DMFNet_attention
 from models import DMFNet_csse
-from models import DMFNet_pe, DMFNet_multiattention, DMFNet_attention, DMFNet_singleattention, DMFNet_separate_inputs
+from models import DMFNet_pe, DMFNet_multiattention, DMFNet_attention, DMFNet_singleattention, DMFNet_separate_inputs,\
+    DMFNet_pp_double
 
 
 if __name__ == '__main__':
     # os.environ['CUDA_VISIBLE_DEVICES'] = '0'
     # device = torch.device('cuda:0')
-    x = torch.rand((1, 4, 240, 240, 160))  # [bsize,channels,Height,Width,Depth]
+    x = torch.rand((1, 4, 128, 128, 128))  # [bsize,channels,Height,Width,Depth]
     # dmf = DMFNet(c=4, groups=16, norm='sync_bn', num_classes=4)
     # dmfpp = DMFNet_pp(c=4, groups=16, norm='sync_bn', num_classes=4)
     # dmfppd = DMFNet_ppd(c=4, groups=16, norm='sync_bn', num_classes=4)
     # dmffull = DMFNet_fullpp(c=4, groups=16, norm='sync_bn', num_classes=4)
-    model = DMFNet_attention(c=4, groups=16, channels=16, norm='sync_bn', num_classes=4)
+    model = DMFNet_pp_double(c=4, groups=16, channels=16, norm='sync_bn', num_classes=4)
     # dmfpe = DMFNet_pe(c=4, groups=16, norm='sync_bn', num_classes=4)
     # dmf_single_att = DMFNet_singleattention(c=4, groups=16, norm='sync_bn', num_classes=4)
     # dmf_multi_att = DMFNet_multiattention(c=4, groups=16, norm='sync_bn', num_classes=4)
